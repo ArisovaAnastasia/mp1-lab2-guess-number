a5c628de1ba05a983ed10f344a8a228bfbf2ef9e
uf0logic <ufologic@protonmail.com>
2022-10-06 20:45:44 +0300

    Slightly Polished the code

--- main.c
+++ main.c
@@ -2,68 +2,61 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
-{
+int main() {
     int mode;
-    char hint; 
-    int n, a, attempts;
+    int n, a, attempts = 0;
     int lower = 1, upper = 1000;
 
     time_t t;
-    srand((unsigned)time(&t));
-    
+    srand((unsigned) time(&t));
+
     do {
         printf("Choose the mode\n");
         scanf("%d", &mode);
-    } 
-    while (mode < 1 || mode > 2);
+    } while (mode < 1 || mode > 2);
 
-    switch (mode)
-    {
-    case 1:
-        n = 1 + rand() % 1000;
-        printf("Enter your option %d\n", n);
-        do
-        {
-            scanf("%d", &a);
-            if (a < n)
-                printf("Number is greater\n");
-            else if (a > n)
-                printf("Number is less\n");
-            attempts++;
-        } while (a != n);
-        printf("Nice! You Guessed!!! The number was %d\n Number of attempts: %d", a, attempts);
+    switch (mode) {
+        case 1:
+            n = 1 + rand() % 1000;
+            printf("Enter your option:\n");
+            do {
+                scanf("%d", &a);
+                if (a < n)
+                    printf("Number is greater\n");
+                else if (a > n)
+                    printf("Number is less\n");
+                attempts++;
+            } while (a != n);
+            printf("Nice! You Guessed!!! The number was %d\nNumber of attempts: %d", a, attempts);
 
-        break;
+            break;
 
-    case 2:
-        do {
-            printf("Enter your option\n");
-            scanf("%d",&n);
-        }
-        while (n < 1 || n > 1000);
+        case 2:
+            do {
+                printf("Enter your option\n");
+                scanf("%d", &n);
+            } while (n < 1 || n > 1000);
 
-        do {
-            a = lower + rand() % (upper - lower + 1);
-            printf("Is it %d ???\n",a);
-            getchar();
+            do {
+                a = lower + rand() % (upper - lower + 1);
+                printf("Is it %d ???\n", a);
+                getchar(); //Без него почему-то два раза выводит предположение о числе
 
-            switch (getchar()) {
-                case '<':
-                    upper = a;
-                    break;
+                switch (getchar()) {
+                    case '<':
+                        upper = a;
+                        break;
 
-                case '>':
-                    lower = a;
-                    break;
-                case '=':
-                    printf("Hooray!!! I've Won! The number is %d\n",a);
-                    break;
-            }
-        }
-        while (a != n);
+                    case '>':
+                        lower = a;
+                        break;
+                    case '=':
+                        printf("Hooray!!! I've Won! The number is %d\n", a);
+                        break;
+                }
+            } while (a != n);
 
-        break;
+            break;
 
     }
 
