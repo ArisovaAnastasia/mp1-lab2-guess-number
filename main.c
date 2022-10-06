638c6937f2a982c35882ffffb91f94edf2b78dfc
Arsenii Goriachev <gospodinufolog@gmail.com>
2022-10-06 14:28:59 +0300

    Competed 2nd mode

--- main.c
+++ main.c
@@ -5,7 +5,10 @@
 int main()
 {
     int mode;
+    char hint; 
     int n, a, attempts;
+    int lower = 1, upper = 1000;
+
     time_t t;
     srand((unsigned)time(&t));
     
@@ -26,14 +29,42 @@ int main()
             if (a < n)
                 printf("Number is greater\n");
             else if (a > n)
-                printf("Number is lower\n");
+                printf("Number is less\n");
+            attempts++;
         } while (a != n);
-        printf("Nice! You Won!!! The number was %d\n", a);
+        printf("Nice! You Guessed!!! The number was %d\n Number of attempts: %d", a, attempts);
+
         break;
 
     case 2:
-        //do printf("Enter your option\n"); scanf("%d",&)
+        do {
+            printf("Enter your option\n");
+            scanf("%d",&n);
+        }
+        while (n < 1 || n > 1000);
+
+        do {
+            a = lower + rand() % (upper - lower + 1);
+            printf("Is it %d ???\n",a);
+            getchar();
+
+            switch (getchar()) {
+                case '<':
+                    upper = a;
+                    break;
+
+                case '>':
+                    lower = a;
+                    break;
+                case '=':
+                    printf("Hooray!!! I've Won! The number is %d\n",a);
+                    break;
+            }
+        }
+        while (a != n);
+
         break;
+
     }
 
     return 0;
