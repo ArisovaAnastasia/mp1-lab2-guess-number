ec4ad07a21f6813438f563b91e55c101ff2df7d0
EgorDormidontov <113050823+EgorDormidontov@users.noreply.github.com>
2022-10-11 13:46:20 +0300

    Add files via upload

---
+++ Source2.cpp
@@ -0,0 +1,55 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<locale.h>
+#include<time.h>
+
+int main()
+{
+    srand(time(NULL));
+    setlocale(LC_ALL, "Rus");
+    int Mod, answer;
+    printf("Если хотите угадывать число введите 1,если хотите чтобы компьютер угадал ваше число введите 2\n");
+    scanf_s("%i", &Mod);
+    if (Mod == 1)
+    {
+        int numb = 1 + rand() % (1000 - 1 + 1);
+        printf("Угадайте случайное число от 1 до 1000\n");
+        while (1)
+        {
+            printf("Введите догадку\n");
+            scanf_s("%i", &answer);
+            if (answer > numb) { printf("Введённое число больше загаданного\n"); }
+            else if (answer < numb) { printf("Введённое число меньшне загаданного\n"); }
+            else if (answer == numb) { printf("Вы угадали число!\n"); break; }
+        }
+    }
+    else
+    {
+        int numb;
+        while (1)
+        {
+            
+            printf("Введите число от 1 до 1000\n");
+            scanf_s("%i", &numb);
+            if ((numb < 1001) && (numb > 0)) { break; }
+        }
+        int rightedge, leftedge;
+        rightedge = 1000;
+        leftedge = 1;
+        while (1)
+        {
+            int answer = leftedge + rand() % (rightedge - leftedge + 1);
+            char s;
+            printf("%i", answer);
+            printf("- загаданное число меньше или больше?А может это оно и есть?(введите <,>,=\n");
+            getchar();
+            scanf_s("%c", &s);
+            if (s == '>') { leftedge = answer; }
+            else if (s == '<') { rightedge = answer; }
+            else if (s == '=') { printf("Я так и знал!"); break; }
+
+        }
+
+    }
+    return 0;
+}
