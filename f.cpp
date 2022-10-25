f464c1fecaa57994efb1785f7f1c03c66e410210
viol8888 <bruskovavioletta@yandex.ru>
2022-10-25 23:28:44 +0300

    jj

--- f.cpp
+++ f.cpp
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-void main() {
+int main() {
 	srand(time(NULL));
 
-	int a, b,d,k,p;
-	char f,l;
+	int a, b, d, k, p;
+	char f;
 	b = 0;
 	k = 1000;
 	p = 0;
-	printf("vvedite nomer regima");
+	printf("enter the mode number");
 	scanf_s("%c", &f);
 	switch (f) {
 	case '1':
-		 d = 0 + rand() % (1000 - 0 + 1);
-		printf("vvedite chislo");
-			scanf_s("%i", &a);
+		d = 0 + rand() % (1000 - 0 + 1);
+		printf("enter a number");
+		scanf_s("%i", &a);
 		while (a != d)
 		{
 			if (a < d)
 			{
-				printf("zagadannoe chislo bolshe");
-				scanf_s("%i",&a);
+				printf("the specified number is greater ");
+				scanf_s("%i", &a);
 				b += 1;
 			}
 			else if (a > d) {
-				printf("zagadannoe chislo menshe");
-				scanf_s("%i",&a);
+				printf("the specified number is less ");
+				scanf_s("%i", &a);
 				b += 1;
 			}
 
@@ -36,28 +36,54 @@ void main() {
 		break;
 
 	case '2':
-		printf("vvedite zagadannoe chislo");
-		scanf_s("%i" ,&a);
-		d = 0;
-		printf("vvedite znak");
-		scanf_s("%c", &l);
-		switch (l) {
-		case '<':
-			k = d;
-			d = k + rand() % (p- k+ 1);
-			b += 1;
-			break;
-		case '>':
-			p = d;
-			d = k + rand() % (p - k + 1);
-			b += 1;
-			break;
-		case '=':
-			printf("%i", b-1);
-			break;
-			
+		printf("enter a number");
+		scanf_s("%i", &a);
+		if (a > 1000 || a < 0) {
+			printf("invalid number");
 		}
-		
-	}	
+		else {
+			int h = 0 + rand() % (1000 - 0 + 1);
+			char l;
+			printf("%i", h);
+			while (h != a) {
+				printf("enter a sign");
+				getchar();
+				scanf_s("%c", &l);
+				switch (l) {
+				case '<':
+					k = h-1;
+					h = p + rand() % (k - p + 1);
+					b += 1;
+					if (h == a) {
+						break;
+					}
+					else {
+						printf("%i ", h);
+						break;
+					}
+						break;
+					
+				case'>':
+					p = h+1;
+					h = p + rand() % (k - p + 1);
+					b += 1;
+					if (h == a) {
+						break;
+					}
+					else {
+						printf("%i ", h);
+						break;
+					}
+					break;
+					}
+				}
+			printf("%i ", h);
+			printf(",");
+				printf("%i ", b);
+				printf("attempts");
 
-}
+			}
+
+		}
+
+	}
